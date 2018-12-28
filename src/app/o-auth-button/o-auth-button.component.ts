b50ecd41f48e19504be3b3ff39fb7df1a9d3c955
Tilak Patel <ptilak142@gmail.com>
2018-12-28 12:53:13 -0500

    working oauth button animation

--- src/app/o-auth-button/o-auth-button.component.ts
+++ src/app/o-auth-button/o-auth-button.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-o-auth-button',
   templateUrl: './o-auth-button.component.html',
   styleUrls: ['./o-auth-button.component.css']
 })
 export class OAuthButtonComponent implements OnInit {
-
+  spinning: string = "";
   constructor() { }
 
   ngOnInit() {
+  }
+
+  doAuth(){
+    this.spinning = "fa-spin";
+    window.location.href = "https://fantasynamegeneratorbackend.herokuapp.com/auth";
   }
 
 }
