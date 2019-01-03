0d42eed67dfbcf6e91775e869aba561823136b9f
Tilak Patel <ptilak142@gmail.com>
2019-01-02 23:39:50 -0500

    faster load to oauth page

--- src/app/o-auth-button/o-auth-button.component.ts
+++ src/app/o-auth-button/o-auth-button.component.ts
@@ -13,7 +13,7 @@ export class OAuthButtonComponent implements OnInit {
 
   doAuth(){
     this.spinning = "fa-spin";
-    window.location.href = "https://fantasynamegeneratorbackend.herokuapp.com/auth";
+    window.location.href = "https://api.login.yahoo.com/oauth2/request_auth?response_type=code&client_id=dj0yJmk9UlVqOW9lWURBSThnJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTNl&redirect_uri=https%3A%2F%2Ffantasynamegeneratorbackend.herokuapp.com%2Fcallback&scope=&state=3(%230%2F!~";
   }
 
 }
